049f57b1aa2ab8c5e9826b088019f457b6d3ec5f
AlanVazquez99 <alanvazquez1999@gmail.com>
2021-01-25 06:49:29 +0000

    NEW: Validate user group to render different templates

--- systemtest/pts/views/recive.py
+++ systemtest/pts/views/recive.py
@@ -8,8 +8,14 @@ from systemtest.pts import models as pts_models, forms as pts_forms
 
 class RecivePartListView(ListView):
     template_name = "pts/recive.html"
-    queryset = pts_models.Request.objects.filter(request_status__pk=3)
+    queryset = pts_models.Request.objects.filter(request_status__name="RECIVE")
 
+    def get_template_names(self) -> list[str]:
+        user_groups = self.request.user.groups.all()
+        if user_groups.filter(name="TA"):
+            self.template_name = "pts/recive_ta.html"
+
+        return super().get_template_names()
 
 class UpdateRequestView(UpdateView):
     model = pts_models.Request
--- systemtest/pts/views/transit.py
+++ systemtest/pts/views/transit.py
@@ -11,5 +11,12 @@ class TransitRequestView(BaseRequestListView):
     template_name = "pts/transit.html"
     success_url = reverse_lazy("pts:transit")
 
-    query = Q(request_status__pk=2)
-    next_status_query = Q(pk=3)
+    query = Q(request_status__name="TRANSIT")
+    next_status_query = Q(name="RECIVE")
+
+    def get_template_names(self) -> list[str]:
+        user_groups = self.request.user.groups.all()
+        if user_groups.filter(name="TA"):
+            self.template_name = "pts/transit_ta.html"
+
+        return super().get_template_names()
