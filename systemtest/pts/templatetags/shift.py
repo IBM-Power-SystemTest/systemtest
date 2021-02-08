537b60caab0c78bda46473e6167856fe8301694b
AlanVazquez99 <alanvazquez1999@gmail.com>
2021-02-08 00:08:27 +0000

    FIX: Use localtime to pass to templatetag shift instead UTC

--- systemtest/pts/templatetags/shift.py
+++ systemtest/pts/templatetags/shift.py
@@ -5,13 +5,13 @@ register = Library()
 
 
 @register.filter("get_shift")
-def get_shift(date: datetime) -> int:
+def get_shift(date: datetime) -> str:
     hour = date.hour
     if hour >= 7 and hour <= 14:
-        return 1
+        return "1"
     elif hour >= 15 and hour <= 22:
-        return 2
-    elif hour >= 23 and hour <= 6:
-        return 3
+        return "2"
+    elif (hour >= 0 and hour <= 7) or hour >= 23:
+        return "3"
     else:
-        return 0
+        return "?"
