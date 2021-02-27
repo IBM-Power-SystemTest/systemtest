139db53bb3b8d3cc7316b345aa933711f4773787
AlanVazquez99 <alanvazquez1999@gmail.com>
2021-02-27 12:13:56 -0600

    IMPROVE: Use podman insead docker and remove traefik

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,23 @@
 #! /bin/bash
+podman pod create --name systemtest -p 80:5000 -p 5555:5555
 
-docker network create --attachable systemtest_network
+podman volume create systemtest_postgres_data
+podman volume create systemtest_postgres_data_backups
 
-docker volume create systemtest_postgres_data
-docker volume create systemtest_postgres_data_backups
-docker volume create systemtest_traefik_data
+podman build -t systemtest_postgres:beta -f ./compose/production/postgres/Dockerfile .
+podman build -t systemtest_django:beta -f ./compose/production/django/Dockerfile .
+podman build -t systemtest_celeryworker:beta -f ./compose/production/django/Dockerfile .
+podman build -t systemtest_celerybeat:beta -f ./compose/production/django/Dockerfile .
+podman build -t systemtest_flower:beta -f ./compose/production/django/Dockerfile .
 
-docker build -t systemtest_django:beta -f ./compose/production/django/Dockerfile .
-docker build -t systemtest_celeryworker:beta -f ./compose/production/django/Dockerfile .
-docker build -t systemtest_celerybeat:beta -f ./compose/production/django/Dockerfile .
-docker build -t systemtest_flower:beta -f ./compose/production/django/Dockerfile .
-docker build -t systemtest_postgres:beta -f ./compose/production/postgres/Dockerfile .
-docker build -t systemtest_traefik:beta -f ./compose/production/traefik/Dockerfile .
+podman run -d --pod systemtest --name postgres -v systemtest_postgres_data:/var/lib/postgresql/data:Z -v systemtest_postgres_data_backups:/backups:z --env-file ./.envs/.production/.postgres systemtest_postgres:beta
 
-docker run -d --network systemtest_network --name postgres -v systemtest_postgres_data:/var/lib/postgresql/data:Z -v systemtest_postgres_data_backups:/backups:z --env-file ./.envs/.production/.postgres systemtest_postgres:beta
+podman run -d --pod systemtest --name redis redis:6.0.9
 
-docker run -d --network systemtest_network --name redis redis:6.0.9
+podman run -d --pod systemtest --name django --env-file ./.envs/.production/.postgres --env-file ./.envs/.production/.django systemtest_django:beta /start
 
-docker run -d --network systemtest_network --name django --env-file ./.envs/.production/.postgres --env-file ./.envs/.production/.django systemtest_django:beta /start
+podman run -d --pod systemtest --name celeryworker --env-file ./.envs/.production/.postgres --env-file ./.envs/.production/.django systemtest_django:beta /start-celeryworker
 
-docker run -d --network systemtest_network --name celeryworker --env-file ./.envs/.production/.postgres --env-file ./.envs/.production/.django systemtest_django:beta /start-celeryworker
+podman run -d --pod systemtest --name celerybeat --env-file ./.envs/.production/.postgres --env-file ./.envs/.production/.django systemtest_django:beta /start-celerybeat
 
-docker run -d --network systemtest_network --name celerybeat --env-file ./.envs/.production/.postgres --env-file ./.envs/.production/.django systemtest_django:beta /start-celerybeat
-
-docker run -d --network systemtest_network --name flower --env-file ./.envs/.production/.postgres --env-file ./.envs/.production/.django systemtest_django:beta /start-flower
-
-docker run -d --network systemtest_network --name traefik -p 80:80 -p 443:443 -p 5555:5555 -v systemtest_traefik_data:/etc/traefik/acme:z systemtest_traefik:beta
+podman run -d --pod systemtest --name flower --env-file ./.envs/.production/.postgres --env-file ./.envs/.production/.django systemtest_django:beta /start-flower
