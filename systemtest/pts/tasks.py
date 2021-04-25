5ecf947a00ccf7826547c26ac11e5e0af6df8cc3
AlanVazquez99 <alanvazquez1999@gmail.com>
2021-04-25 18:32:15 +0000

    NEW: Request close as bad with celery tasks

--- systemtest/pts/tasks.py
+++ systemtest/pts/tasks.py
@@ -15,20 +15,28 @@ from systemtest.pts import models as pts_models
 database = Database(**settings.DATABASES.get("db2"))
 
 
-def get_ncm(pending_part: pts_models.Request) -> Union[dict[str, str], None]:
+def get_pending_request():
+    return pts_models.Request.objects.filter(request_status__name="PENDING")
+
+
+def get_bad_request():
+    return pts_models.Request.objects.filter(request_status__name="BAD")
+
+
+def get_ncm(request: pts_models.Request) -> Union[dict[str, str], None]:
     data = {
-        "part_number": pending_part.part_number,
-        "serial_number": pending_part.serial_number,
-        "created": pending_part.created
+        "part_number": request.part_number,
+        "serial_number": request.serial_number,
+        "created": request.created
     }
 
     sql = """
         SELECT
-            RPREJS AS NCM_TAG
+            RPREJS AS NCM_TAG,
+            RPCULC AS CURRENT_LOCATION
         FROM
             QRYQ.MFSGPRP10_GRC
         WHERE
-            RPCULC = 'PDBG' AND
             RPITEM = '00000{part_number}' AND   -- Part number has 12 chars,
                                                 -- 5 chars aditional to save in db,
                                                 -- so filled extra chars with 0s
@@ -39,29 +47,41 @@ def get_ncm(pending_part: pts_models.Request) -> Union[dict[str, str], None]:
     rows = database.fetch(sql)
     row = next(rows, {})
 
+    print(data)
+    print(row)
+
     if ncm_tag := row.get("NCM_TAG"):
         data["ncm_tag"] = ncm_tag
-        del data["created"]
-        return data
 
+        row = next(rows, {})
+        data["is_returned"] = row.get("CURRENT_LOCATION", "").strip() == "PNCM"
+
+        return data
     return None
 
 
+def return_bad(request: pts_models.Request, ncm_data: dict[str, Any]):
+    if ncm_data:
+        bad_status = pts_models.RequestStatus.objects.get(
+            name="BAD")
+
+        if request.request_status != bad_status:
+            request.__dict__.update(ncm_data)
+            request.save()
+
+        if ncm_data.get("is_returned"):
+            request.request_status = pts_models.RequestStatus.objects.get(
+                name="CLOSE BAD")
+            request.save()
+
+
 @app.task()
-def return_ncm():
-    query = (
-        Q(request_status__name="PENDING") |
-        Q(request_status__name="INSTALADO EN OTRA WU") |
-        Q(request_status__name="REVISION CON EL ME")
-    )
+def looking_bad():
+    for pending_request in get_pending_request():
+        if ncm_data := get_ncm(pending_request.get_first_request()) is None:
+            ncm_data = get_ncm(pending_request)
+        return_bad(pending_request, ncm_data)
 
-    for pending_part in pts_models.Request.objects.filter(query):
-        if ncm_data := get_ncm(pending_part.get_first_request()) is None:
-            ncm_data = get_ncm(pending_part)
-
-        if ncm_data:
-            pending_part.__dict__.update(ncm_data)
-            pending_part.request_status = pts_models.RequestStatus.objects.get(
-                name="BAD")
-
-            pending_part.save()
+    for bad_part in get_bad_request():
+        ncm_data = get_ncm(bad_part)
+        return_bad(bad_part, ncm_data)
